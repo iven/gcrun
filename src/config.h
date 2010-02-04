7722d09014a612e330edce7782c14ca2e5e0a960
Iven <ivenvd@gmail.com>
2010-02-04 22:44:29 +0800

    First commit.

---
+++ src/config.h
@@ -0,0 +1,20 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+
+#define APP_NAME "gcrun"
+#define APP_VERSION "0.0.1"
+#define APP_URL ""
+
+#define GC_DATA_DIR "/usr/share/gcrun"
+//#define GC_PIXMAPS_DIR ""
+#define GC_DOC_DIR "/usr/share/doc/gcrun"
+#define GC_LOCALE_DIR "/usr/share/locale"
+
+#define GC_GETTEXT_PACKAGE "gcrun"
+#define GC_CONFIG_FILE_NAME "gcrunrc"
+//#define GC_LINK_OPEN_CMD "xdg-open"
+
+#endif
