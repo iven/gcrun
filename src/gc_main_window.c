a1f6eac1a6d031ed2146ef510234486fac384f2a
Iven <ivenvd@gmail.com>
2010-02-19 23:30:47 +0800

    Added url_parse and str_replace, need further work.

--- src/gc_main_window.c
+++ src/gc_main_window.c
@@ -16,22 +16,60 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include	<string.h>
 #include	"gc_main_window.h"
 #include	"gc_entry.h"
 #include	"gc_config.h"
 
+static gchar *gc_str_replace(const gchar *text,
+        const gchar *before, const gchar *after)
+{
+    GString *string;
+    gchar *p_str, *p_found;
+    string = g_string_new("");
+    p_str = text;
+    p_found = g_strstr_len(p_str, -1, before);
+    while (p_found != NULL) {
+        g_string_append_len(string, p_str, p_found - p_str);
+        g_string_append(string, after);
+        p_str = p_found + 2;
+        p_found = g_strstr_len(p_str, -1, before);
+    }
+    g_string_append(string, p_str);
+    p_str = string->str;
+    g_string_free(string, FALSE);
+    return p_str;
+}
+
+static gchar *gc_prot_parse(const gchar *text)
+{
+    gchar *scheme, *replaced_u, *replaced_s;
+    const gchar *handler;
+
+    scheme = g_uri_parse_scheme(text);
+    handler = gc_config_get_string_join("Prot", scheme, "");
+    replaced_u = gc_str_replace(handler, "%u", text);
+    replaced_s = gc_str_replace(replaced_u, "%s", text + strlen(scheme) + 1);
+
+    g_free(replaced_u);
+    g_free(scheme);
+    return replaced_s;
+}
+
 static gboolean on_destroy(GtkWidget *entry, gpointer data)
 {
     gtk_main_quit();
-
     return FALSE;
 }
 
 static gboolean on_activate(GtkWidget *entry, gpointer data)
 {
-    g_spawn_command_line_async(gtk_entry_get_text(GTK_ENTRY(entry)), NULL);
-
-    g_signal_emit_by_name(entry, "destroy");
+    const gchar *text = gtk_entry_get_text(GTK_ENTRY(entry));
+    gchar *str = gc_prot_parse(text);
+    g_print("%s\n", str);
+    g_free(str);
+//    g_spawn_command_line_async(string->str, NULL);
+//    g_signal_emit_by_name(entry, "destroy");
     return TRUE;
 }
 
@@ -70,7 +108,7 @@ GtkWidget *gc_main_window_new(gchar *geometry)
     gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
 
     GtkWidget *entry = gc_entry_new();
-    g_signal_connect(entry, "activate", G_CALLBACK(on_activate), FALSE);
+    g_signal_connect(entry, "activate", G_CALLBACK(on_activate), NULL);
     g_signal_connect(entry, "destroy", G_CALLBACK(on_destroy), NULL);
     gtk_widget_show(entry);
     gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
